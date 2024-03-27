789743795422b721e4b32cabe8e6bcdf7b97f1f0
GiulioUmbrella <giulio.umbrella@gmail.com>
2024-03-27 03:41:44 -0700

    Add union for additional info

--- inc/IRPLogger.h
+++ inc/IRPLogger.h
@@ -59,6 +59,25 @@ typedef _Return_type_success_(return >= 0) LONG NTSTATUS;
 #define RECORD_TYPE_FLAG_OUT_OF_MEMORY           0x10000000
 #define RECORD_TYPE_FLAG_MASK                    0xffff0000
 
+
+
+typedef union _X {
+    
+    struct {
+        LARGE_INTEGER ByteOffset;
+    } Write;
+    struct 
+    {
+        LARGE_INTEGER ByteOffset;
+    } Read;
+    struct 
+    {
+        int FileInformationClass;
+    } SetInformation;
+    
+} X, *PX;
+
+
 //  The fixed data received for RECORD_TYPE_NORMAL
 typedef struct _RECORD_DATA {
     LARGE_INTEGER originating_time;
@@ -86,6 +105,7 @@ typedef struct _RECORD_DATA {
     ULONG ecp_count;
     ULONG known_ecp_mask;
 	double entropy;
+    X x;
 } RECORD_DATA, *PRECORD_DATA;
 
 //  What information we actually log.
